aeac177e661a53170a9f9b386ebb9596401d34c3
Sudhir Yadav <sk14395257@gmail.com>
2026-09-21 21:38:51 +0530

    welcome program

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello worlo");
+        System.out.println("This is the first code");
+        System.out.println("We are starting 30 day challenge");
+        System.out.println("Be coutinuous");
+        System.out.println("Be disipline if you want success");
+        //This is my production Day 1/30
+        
+    }
+}
